b7e4f9d0653ac508ac24b4e8e0e36f96aefabe32
Tanjoodo <tanjoodo@hotmail.com>
2014-07-30 01:23:10 +0300

    another hack to make a pause screen

---
+++ include/globals.h
@@ -0,0 +1,9 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+KB_State kbstate;
+SDL_Renderer *ren = nullptr;
+SDL_Color white = {255,255,255};
+SDL_Event e;
+
+#endif // GLOBALS_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include "../include/ball.h"
 #include "../include/kb_state.h"
 #include "../include/menu_option.h"
+#include "../include/globals.h"
 #include <sstream>
 
-void menu();
+bool menu();
 
 int main(int argc, char** argv){
 	if (SDL_Init(SDL_INIT_EVERYTHING) == -1){
@@ -22,7 +23,6 @@ int main(int argc, char** argv){
         return 1;
     }
 
-	SDL_Renderer *ren = nullptr;
 	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == nullptr){
 		std::cout << SDL_GetError() << std::endl;
@@ -41,34 +41,105 @@ int main(int argc, char** argv){
     paddle1.Setx(0 + 5);                      paddle2.Setx(SCREEN_WIDTH - w2  - 5);
     paddle1.Sety(SCREEN_HEIGHT / 2 - h1/2);   paddle2.Sety(SCREEN_HEIGHT / 2 - h2/2);
 
-    KB_State kbstate;
 
     Ball ball;
     ball.SetTexture("assets/ball.png", ren);
     ball.Setx(SCREEN_WIDTH / 2 - 16);
     ball.Sety(SCREEN_HEIGHT / 2 - h1/2);
 
-    SDL_Color white = {255,255,255};
+
     SDL_Texture *scoretext = renderText("0      0", "assets/alterebro-pixel-font.ttf", white, 150, ren);
     std::ostringstream ss;
     int score_w, score_h;
     SDL_QueryTexture(scoretext, NULL, NULL, &score_w, &score_h);
 
+    bool quit = menu();
+
+    bool pause = false;
+    while (!quit)
+    {
+        //Update
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT) quit = true;
+            if(e.type == SDL_KEYDOWN || e.type == SDL_KEYUP)
+                kbstate.Update(e);
+        }
+
+        if (!pause)
+        {
+            if (kbstate.GetKey(SDL_SCANCODE_W))
+                paddle1.MoveY(-5);
+            if (kbstate.GetKey(SDL_SCANCODE_S))
+                paddle1.MoveY(5);
+            if (kbstate.GetKey(SDL_SCANCODE_UP))
+                paddle2.MoveY(-5);
+            if (kbstate.GetKey(SDL_SCANCODE_DOWN))
+                paddle2.MoveY(5);
+            if (kbstate.GetKey(SDL_SCANCODE_ESCAPE))
+                quit = menu();
+
+
+            int ball_status = ball.Update(paddle1.GetAABB(), paddle2.GetAABB());
+            if (ball_status == 1)
+            {
+                paddle2.UpScore();
+            }
+            else if (ball_status == 2)
+            {
+                paddle1.UpScore();
+            }
+
+            if (ball_status)
+            {
+                SDL_DestroyTexture(scoretext); //We don't need the old texture anymore.
+                ss.str(std::string());
+                ss << paddle1.GetScore() << "      " << paddle2.GetScore();
+                scoretext = renderText(ss.str(), font,white,150,ren);
+                //SDL_QueryTexture(scoretext, NULL, NULL, &score_w, &score_h);
+            }
+        }
+
+
+        //Draw
+        SDL_RenderClear(ren);
+        if (pause)
+        {
+
+        }
+        renderTexture(scoretext, ren, SCREEN_WIDTH / 2 - score_w / 2, 0);
+        paddle1.Draw(ren);
+        paddle2.Draw(ren);
+        ball.Draw(ren);
+        SDL_RenderPresent(ren);
+
+
+    }
+
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+
+	SDL_Quit();
+	return 0;
+}
+
+bool menu()
+{
     bool play = false;
     bool quit = false;
     SDL_Texture *selection = loadTexture("assets/selection.png", ren);
     int selec_location = 0; //I know it's missing a t, I'll leave it this way.
     int max_select = 1;
 
+    bool prev_down_key = false;
+    bool prev_up_key = false;
+
     SDL_Texture *play_text = renderText("Play","assets/alterebro-pixel-font.ttf", white, 64, ren);
     SDL_Texture *exit_text = renderText("Exit","assets/alterebro-pixel-font.ttf", white, 64, ren);
     MenuOption play_option(play_text, SCREEN_WIDTH / 2 - 128, 128);
     MenuOption exit_option(exit_text, SCREEN_WIDTH / 2 - 128, 128 + 64);
 
-    bool prev_down_key = false;
-    bool prev_up_key = false;
 
-    SDL_Event e;
     while (!play)
     {
        while (SDL_PollEvent(&e))
@@ -128,61 +199,5 @@ int main(int argc, char** argv){
         SDL_RenderPresent(ren);
     }
 
-
-    while (!quit)
-    {
-        while (SDL_PollEvent(&e))
-        {
-            if (e.type == SDL_QUIT) quit = true;
-            if(e.type == SDL_KEYDOWN || e.type == SDL_KEYUP)
-                kbstate.Update(e);
-        }
-
-        if (kbstate.GetKey(SDL_SCANCODE_W))
-            paddle1.MoveY(-5);
-        if (kbstate.GetKey(SDL_SCANCODE_S))
-            paddle1.MoveY(5);
-        if (kbstate.GetKey(SDL_SCANCODE_UP))
-            paddle2.MoveY(-5);
-        if (kbstate.GetKey(SDL_SCANCODE_DOWN))
-            paddle2.MoveY(5);
-        if (kbstate.GetKey(SDL_SCANCODE_ESCAPE))
-            quit = true;
-
-        int ball_status = ball.Update(paddle1.GetAABB(), paddle2.GetAABB());
-        if (ball_status == 1)
-        {
-            paddle2.UpScore();
-        }
-        else if (ball_status == 2)
-        {
-            paddle1.UpScore();
-        }
-
-        if (ball_status)
-        {
-            SDL_DestroyTexture(scoretext); //We don't need the old texture anymore.
-            ss.str(std::string());
-            ss << paddle1.GetScore() << "      " << paddle2.GetScore();
-            scoretext = renderText(ss.str(), font,white,150,ren);
-            //SDL_QueryTexture(scoretext, NULL, NULL, &score_w, &score_h);
-        }
-
-
-
-        SDL_RenderClear(ren);
-        renderTexture(scoretext, ren, SCREEN_WIDTH / 2 - score_w / 2, 0);
-        paddle1.Draw(ren);
-        paddle2.Draw(ren);
-        ball.Draw(ren);
-        SDL_RenderPresent(ren);
-
-
-    }
-
-	SDL_DestroyRenderer(ren);
-	SDL_DestroyWindow(win);
-
-	SDL_Quit();
-	return 0;
+    return quit;
 }
